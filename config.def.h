73430f80391ccf29015b5332f4518f2b6cd2954f
Chris Oldmeadow <chris.oldmeadow@protonmail.com>
2021-01-12 12:18:59 +1100

    Added Font Awesome for the tag characters - needs nerd-fonts-complete on Arch

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Noto sans:size=10" };
-static const char dmenufont[]       = "Noto sans:size=10";
+static const char *fonts[]          = { "Ubuntu Nerd Font:size=10" };
+static const char dmenufont[]       = "Ubuntu Mono Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -39,7 +39,8 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", " "};
+/*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -86,7 +87,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return,      spawn,     SHCMD("thunar")},
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_r,      spawn,     SHCMD("rofi -show run") },
